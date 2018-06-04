f5f8e20ae474a809e72e620d4fad626b89b8076a
maosilu <1540688711@qq.com>
2018-06-04 15:16:03 +0800

    多维数组转XML

--- 12arrxml.php
+++ 12arrxml.php
@@ -27,3 +27,30 @@ EOT;
 header('Content-type:text/xml');
 echo arr2xml($arr);*/
 
+/*
+多维数组转xml
+*/
+$arr = array(
+	'name' => 'Jhon',
+	'age' => 29,
+	'job' => array(
+		'title' => 'Manager',
+		'salary' => 9000,
+		'team' => array('hh', 'gg', 'yy'),
+	),
+);
+
+function arr2xml(Array $arr){
+	$xml = new SimpleXMLElement('<?xml version="1.0" encoding="utf-8" ?><root></root>');
+	foreach($arr as $k => $v){
+		if(is_array($v)){
+			$xml->addChild($k);
+			arr2xml($v);
+		}else{
+			$xml->addChild($k, $v);
+		}
+	}
+	return $xml->asXML();
+}
+header('Content-type:text/xml');
+echo arr2xml($arr);
